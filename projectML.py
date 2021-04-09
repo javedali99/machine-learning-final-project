6e753afc57cc819da0e8fa453afc68bba4f5dfcf
AmandaBarroso <77797022+AmandaBarroso@users.noreply.github.com>
2021-04-09 12:23:43 -0400

    Update projectML.py

--- projectML.py
+++ projectML.py
@@ -1,9 +1,9 @@
-
 #  Created by Amanda Barroso on 3/26/21.
-#  
 import scipy.io as sio
 import matplotlib.pyplot as plt
+import pandas as pd
 
+## Load St. Pete Water Level data
 matstruct_contents = sio.loadmat('WaterLevel_St_Pete_hourly.mat')
 
 raw_waterlevel= matstruct_contents['WaterLevel']
@@ -17,3 +17,6 @@ detrended = sio.loadmat('hourlydt.mat')
 dt_water = detrended['hourlydt']
 plt.plot(time,dt_water)
 plt.show()
+
+## Load Cuxhaven storm surge data (because it is already prepared)
+
